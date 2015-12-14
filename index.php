8c78513c7305ef0ab12faff536c9e6badbf82841
johann <johann.creneguy@gmail.com>
2015-12-14 19:03:55 +0100

    add index.php

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>challenge - thinkinnovation</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<div id="main">
+		
+		<header>
+			<div id="logo">
+				<img src="img/logo.png">
+			</div>
+		</header>
+
+		<section class="cream1">
+			<div>
+				<h2>module 1</h2>
+				<video width="100%" controls="controls">
+					<source src="video/video.webm" type="video/webm">
+					<source src="video/video.mp4" type="video/mp4">
+					<source src="video/video.ogv" type="video/ogg">
+					<source src="video/video.3gp" type="video/3gp"/>
+					Your browser does not support the video tag.
+				</video>
+			</div>
+			<div>
+				<h3>What is <span>D</span>ART<span>?</span></h3>
+				<p>This short video (1mn) will introduce you to DART and how it can help you find the documents you are looking for.</p>
+				<p><span>D</span>ART stands for "Document Archive Retrieval Tool" and was built in 2006 by the team with the same name. It is an extraordinary resource for all the BCS Regulatory community.</p>
+			</div>
+			<div id="tips1">
+				<p>Did this video answer your questions?</p>
+				<div>✔</div>
+			</div>
+		</section>
+
+		<section class="grey">
+			<h2>module 2</h2>
+			
+			<h3>How does <span>D</span>ART work?</h3>
+			<p>You are now convinced that DART can be a very precious partner in your daily activities. Want to see what it is like and how easy it is to start working with it? The following demos will show you 7 practical use cases (1mn30 each).</p>
+			
+			<video width="100%" controls="controls" poster="img/poster.png">
+				<source src="video/video.mp4" type="video/mp4">
+			</video>
+			
+			<article class="vignette">
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="video/video.mp4" type="video/mp4">
+				</video>
+				<h4>part 2</h4>
+				<h5>Multiple search</h5>
+			</article>
+
+			<article class="vignette">
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="video/video.mp4" type="video/mp4">
+				</video>
+				<h4>part 3</h4>
+				<h5>Primary document</h5>
+			</article>
+
+			<article class="vignette">
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="video/video.mp4" type="video/mp4">
+				</video>
+				<h4>part 4</h4>
+				<h5>Multiple search</h5>
+			</article>
+
+			<article class="vignette">
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="video/video.mp4" type="video/mp4">
+				</video>
+				<h4>part 5</h4>
+				<h5>Primary document</h5>
+			</article>
+
+			<article class="vignette">
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="video/video.mp4" type="video/mp4">
+				</video>
+				<h4>part 6</h4>
+				<h5>Multiple search</h5>
+			</article>
+
+			<article class="vignette">
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="video/video.mp4" type="video/mp4">
+				</video>
+				<h4>part 7</h4>
+				<h5>Primary document</h5>
+			</article>
+
+			<div id="tips2">
+				<p>Did this video answer your questions?</p>
+				<div>✔</div>
+			</div>
+		</section>
+		
+		<section class="cream2">
+			<h2>module 3</h2>
+			
+			<h3>Getting to know <span>D</span>ART better</h3>
+			<p>Before going further and getting your <span>D</span>ART access rights, there are a few concepts we want you to know.The main DART concepts are illustrated in these 3 short videos (1mn30 each).</p>
+			
+			<article class="vignette">
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="video/video.mp4" type="video/mp4">
+				</video>
+				<h4>part 1</h4>
+				<h5>Primary document and document workflow</h5>
+				<p>This video comes across the concept of document workflow. In <span>D</span>ART, the document workflow is a key functionality to the system.</p>
+			</article>
+
+			<article class="vignette">
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="video/video.mp4" type="video/mp4">
+				</video>
+				<h4>part 2</h4>
+				<h5>Multiple search</h5>
+			</article>
+
+			<article class="vignette">
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="video/video.mp4" type="video/mp4">
+				</video>
+				<h4>part 3</h4>
+				<h5>Multiple search</h5>
+			</article>
+		</section>
+		
+		<section class="red">
+			<p><span>Almost done</span></p>
+			<p>Now that you a pretty good idea of what <span>D</span>ART is and can it do for you, let's do a quick roundup:</p>
+		</section>
+		
+		<section class="cream2">
+			<h2>final round</h2>
+			
+			<h3><span>D</span>ART roundup</h3>
+
+			<form action="#" method="post">
+				<div>
+					<p>What is DART?</p>
+					<label for="answer1"> 
+						<input type="radio" name="question1" id="answer1" value="option1"> answer 1 
+					</label>
+					<label for="answer2"> 
+						<input type="radio" name="question1" id="answer2" value="option2"> answer 2 
+					</label>
+					<label for="answer3"> 
+						<input type="radio" name="question1" id="answer3" value="option3"> answer 3 
+					</label>
+				</div>
+					
+				<div>
+					<p>Which documents can I find in DART?</p>
+					<label for="answer1"> 
+						<input type="radio" name="question2" id="answer1" value="option1"> answer 1 
+					</label>
+					<label for="answer2"> 
+						<input type="radio" name="question2" id="answer2" value="option2"> answer 2 
+					</label>
+					<label for="answer3"> 
+						<input type="radio" name="question2" id="answer3" value="option3"> answer 3 
+					</label>
+				</div>
+
+				<div>
+					<p>What is the purpose of DART?</p>
+					<label for="answer1"> 
+						<input type="radio" name="question3" id="answer1" value="option1"> answer 1 
+					</label>
+					<label for="answer2"> 
+						<input type="radio" name="question3" id="answer2" value="option2"> answer 2 
+					</label>
+					<label for="answer3"> 
+						<input type="radio" name="question3" id="answer3" value="option3"> answer 3 
+					</label>
+				</div>
+
+				<div>
+					<p>What is the interpretation of a DART number?</p>
+					<label for="answer1"> 
+						<input type="radio" name="question4" id="answer1" value="option1"> answer 1 
+					</label>
+					<label for="answer2"> 
+						<input type="radio" name="question4" id="answer2" value="option2"> answer 2 
+					</label>
+					<label for="answer3"> 
+						<input type="radio" name="question4" id="answer3" value="option3"> answer 3 
+					</label>
+				</div>
+
+				<div>
+					<p>Talking about the search for a document by Edition Number, which of the following statements are true?</p>
+					<label for="answer1"> 
+						<input type="radio" name="question5" id="answer1" value="option1"> answer 1 
+					</label>
+					<label for="answer2"> 
+						<input type="radio" name="question5" id="answer2" value="option2"> answer 2 
+					</label>
+					<label for="answer3"> 
+						<input type="radio" name="question5" id="answer3" value="option3"> answer 3 
+					</label>
+				</div>
+
+				<div>
+					<p>How can I open the archived document?</p>
+					<label for="answer1"> 
+						<input type="radio" name="question6" id="answer1" value="option1"> answer 1 
+					</label>
+					<label for="answer2"> 
+						<input type="radio" name="question6" id="answer2" value="option2"> answer 2 
+					</label>
+					<label for="answer3"> 
+						<input type="radio" name="question6" id="answer3" value="option3"> answer 3 
+					</label>
+				</div>
+
+				<div>
+					<p>What do the three columns in front of the query result list mean?</p>
+					<label for="answer1"> 
+						<input type="radio" name="question7" id="answer1" value="option1"> answer 1 
+					</label>
+					<label for="answer2"> 
+						<input type="radio" name="question7" id="answer2" value="option2"> answer 2 
+					</label>
+					<label for="answer3"> 
+						<input type="radio" name="question7" id="answer3" value="option3"> answer 3 
+					</label>
+				</div>
+
+				<div>
+					<input type="submit" value="Save my answers">
+				</div>
+			</form>
+		</section>
+	
+	</div>
+</body>
+</html>
