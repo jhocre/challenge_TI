eefbcf43dfab5fe4e6e7936c94084e62941f08d7
johann <johann.creneguy@gmail.com>
2015-12-17 15:47:52 +0100

    few changes for the new style sheet (new ids and classes)

--- index.php
+++ index.php
@@ -19,17 +19,20 @@
 		</section>
 
 		<section class="cream1">
-			<div>
+			<div id="demo_video">
 				<h2>module 1</h2>
-				<video width="100%" controls="controls">
-					<source src="video/video.webm" type="video/webm">
-					<source src="video/video.mp4" type="video/mp4">
-					<source src="video/video.ogv" type="video/ogg">
-					<source src="video/video.3gp" type="video/3gp">
-					Your browser does not support the video tag.
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="http://techslides.com/demos/sample-videos/small.webm"
+						type="video/webm">
+					<source src="http://techslides.com/demos/sample-videos/small.ogv"
+						type="video/ogg">
+					<source src="http://techslides.com/demos/sample-videos/small.mp4"
+						type="video/mp4">
+					<source src="http://techslides.com/demos/sample-videos/small.3gp"
+						type="video/3gp">
 				</video>
 			</div>
-			<div>
+			<div id="demo_text">
 				<h3>What is <span>D</span>ART<span>?</span></h3>
 				<p>This short video (1mn) will introduce you to DART and how it can help you find the documents you are looking for.</p>
 				<p><span>D</span>ART stands for "Document Archive Retrieval Tool" and was built in 2006 by the team with the same name. It is an extraordinary resource for all the BCS Regulatory community.</p>
@@ -41,17 +44,21 @@
 		</section>
 
 		<section class="grey">
+		<div id="triangle1"></div>
 			<h2>module 2</h2>
 			
 			<h3>How does <span>D</span>ART work?</h3>
 			<p>You are now convinced that DART can be a very precious partner in your daily activities. Want to see what it is like and how easy it is to start working with it? The following demos will show you 7 practical use cases (1mn30 each).</p>
 			<article class="vignette">
-				<video width="100%" controls="controls">
-					<source src="video/video.webm" type="video/webm">
-					<source src="video/video.mp4" type="video/mp4">
-					<source src="video/video.ogv" type="video/ogg">
-					<source src="video/video.3gp" type="video/3gp">
-					Your browser does not support the video tag.
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="http://techslides.com/demos/sample-videos/small.webm"
+						type="video/webm">
+					<source src="http://techslides.com/demos/sample-videos/small.ogv"
+						type="video/ogg">
+					<source src="http://techslides.com/demos/sample-videos/small.mp4"
+						type="video/mp4">
+					<source src="http://techslides.com/demos/sample-videos/small.3gp"
+						type="video/3gp">
 				</video>
 				<div class="details">
 					<h4>part 1</h4>
@@ -59,89 +66,95 @@
 				</div>
 			</article>
 			
-			<article class="vignette">
-				<video width="100%" controls="controls">
-					<source src="video/video.webm" type="video/webm">
-					<source src="video/video.mp4" type="video/mp4">
-					<source src="video/video.ogv" type="video/ogg">
-					<source src="video/video.3gp" type="video/3gp">
-					Your browser does not support the video tag.
-				</video>
-				<div class="details">
-					<h4>part 2</h4>
-					<h5>Multiple search</h5>
-				</div>
-			</article>
+			<div class="container">
+				<article class="vignette" id="n2">
+					<video width="100%" controls="controls">
+						<source src="video/video.webm" type="video/webm">
+						<source src="video/video.mp4" type="video/mp4">
+						<source src="video/video.ogv" type="video/ogg">
+						<source src="video/video.3gp" type="video/3gp">
+						Your browser does not support the video tag.
+					</video>
+					<div class="details">
+						<h4>part 2</h4>
+						<h5>Multiple search</h5>
+					</div>
+				</article>
 
-			<article class="vignette">
-				<video width="100%" controls="controls">
-					<source src="video/video.webm" type="video/webm">
-					<source src="video/video.mp4" type="video/mp4">
-					<source src="video/video.ogv" type="video/ogg">
-					<source src="video/video.3gp" type="video/3gp">
-					Your browser does not support the video tag.
-				</video>
-				<div class="details">
-					<h4>part 3</h4>
-					<h5>Primary document</h5>
-				</div>
-			</article>
+				<article class="vignette" id="n3">
+					<video width="100%" controls="controls">
+						<source src="video/video.webm" type="video/webm">
+						<source src="video/video.mp4" type="video/mp4">
+						<source src="video/video.ogv" type="video/ogg">
+						<source src="video/video.3gp" type="video/3gp">
+						Your browser does not support the video tag.
+					</video>
+					<div class="details">
+						<h4>part 3</h4>
+						<h5>Primary document</h5>
+					</div>
+				</article>
 
-			<article class="vignette">
-				<video width="100%" controls="controls">
-					<source src="video/video.webm" type="video/webm">
-					<source src="video/video.mp4" type="video/mp4">
-					<source src="video/video.ogv" type="video/ogg">
-					<source src="video/video.3gp" type="video/3gp">
-					Your browser does not support the video tag.
-				</video>
-				<div class="details">
-					<h4>part 4</h4>
-					<h5>Multiple search</h5>
-				</div>
-			</article>
+				<article class="vignette" id="n4">
+					<video width="100%" controls="controls">
+						<source src="video/video.webm" type="video/webm">
+						<source src="video/video.mp4" type="video/mp4">
+						<source src="video/video.ogv" type="video/ogg">
+						<source src="video/video.3gp" type="video/3gp">
+						Your browser does not support the video tag.
+					</video>
+					<div class="details">
+						<h4>part 4</h4>
+						<h5>Multiple search</h5>
+					</div>
+				</article>
+				<div style="clear:both;"></div>
+			</div>
 
-			<article class="vignette">
-				<video width="100%" controls="controls">
-					<source src="video/video.webm" type="video/webm">
-					<source src="video/video.mp4" type="video/mp4">
-					<source src="video/video.ogv" type="video/ogg">
-					<source src="video/video.3gp" type="video/3gp">
-					Your browser does not support the video tag.
-				</video>
-				<div class="details">
-					<h4>part 5</h4>
-					<h5>Primary document</h5>
-				</div>
-			</article>
+			<div class="container">
+				<article class="vignette" id="n5">
+					<video width="100%" controls="controls">
+						<source src="video/video.webm" type="video/webm">
+						<source src="video/video.mp4" type="video/mp4">
+						<source src="video/video.ogv" type="video/ogg">
+						<source src="video/video.3gp" type="video/3gp">
+						Your browser does not support the video tag.
+					</video>
+					<div class="details">
+						<h4>part 5</h4>
+						<h5>Primary document</h5>
+					</div>
+				</article>
 
-			<article class="vignette">
-				<video width="100%" controls="controls">
-					<source src="video/video.webm" type="video/webm">
-					<source src="video/video.mp4" type="video/mp4">
-					<source src="video/video.ogv" type="video/ogg">
-					<source src="video/video.3gp" type="video/3gp">
-					Your browser does not support the video tag.
-				</video>
-				<div class="details">
-					<h4>part 6</h4>
-					<h5>Multiple search</h5>
-				</div>
-			</article>
+				<article class="vignette" id="n6">
+					<video width="100%" controls="controls">
+						<source src="video/video.webm" type="video/webm">
+						<source src="video/video.mp4" type="video/mp4">
+						<source src="video/video.ogv" type="video/ogg">
+						<source src="video/video.3gp" type="video/3gp">
+						Your browser does not support the video tag.
+					</video>
+					<div class="details">
+						<h4>part 6</h4>
+						<h5>Multiple search</h5>
+					</div>
+				</article>
 
-			<article class="vignette">
-				<video width="100%" controls="controls">
-					<source src="video/video.webm" type="video/webm">
-					<source src="video/video.mp4" type="video/mp4">
-					<source src="video/video.ogv" type="video/ogg">
-					<source src="video/video.3gp" type="video/3gp">
-					Your browser does not support the video tag.
-				</video>
-				<div class="details">
-					<h4>part 7</h4>
-					<h5>Primary document</h5>
-				</div>
-			</article>
+				<article class="vignette" id="n7">
+					<video width="100%" controls="controls">
+						<source src="video/video.webm" type="video/webm">
+						<source src="video/video.mp4" type="video/mp4">
+						<source src="video/video.ogv" type="video/ogg">
+						<source src="video/video.3gp" type="video/3gp">
+						Your browser does not support the video tag.
+					</video>
+					<div class="details">
+						<h4>part 7</h4>
+						<h5>Primary document</h5>
+					</div>
+				</article>
+				<div style="clear:both;"></div>
+			</div>
 
 			<div id="tips2">
 				<p>Did this video answer your questions?</p>
@@ -155,13 +168,16 @@
 			<h3>Getting to know <span>D</span>ART better</h3>
 			<p>Before going further and getting your <span>D</span>ART access rights, there are a few concepts we want you to know.The main DART concepts are illustrated in these 3 short videos (1mn30 each).</p>
 			
-			<article class="vignette">
-				<video width="100%" controls="controls">
-					<source src="video/video.webm" type="video/webm">
-					<source src="video/video.mp4" type="video/mp4">
-					<source src="video/video.ogv" type="video/ogg">
-					<source src="video/video.3gp" type="video/3gp">
-					Your browser does not support the video tag.
+			<article class="vignette" id="n8">
+				<video width="100%" controls="controls" poster="img/poster.png">
+					<source src="http://techslides.com/demos/sample-videos/small.webm"
+						type="video/webm">
+					<source src="http://techslides.com/demos/sample-videos/small.ogv"
+						type="video/ogg">
+					<source src="http://techslides.com/demos/sample-videos/small.mp4"
+						type="video/mp4">
+					<source src="http://techslides.com/demos/sample-videos/small.3gp"
+						type="video/3gp">
 				</video>
 				<div class="details">
 					<h4>part 1</h4>
@@ -169,42 +185,51 @@
 					<p>This video comes across the concept of document workflow. In <span>D</span>ART, the document workflow is a key functionality to the system.</p>
 				</div>
 			</article>
+			<div id="n9_10">
+				<article class="vignette" id="n9">
+					<video width="100%" controls="controls" poster="img/poster.png">
+						<source src="http://techslides.com/demos/sample-videos/small.webm"
+							type="video/webm">
+						<source src="http://techslides.com/demos/sample-videos/small.ogv"
+							type="video/ogg">
+						<source src="http://techslides.com/demos/sample-videos/small.mp4"
+							type="video/mp4">
+						<source src="http://techslides.com/demos/sample-videos/small.3gp"
+							type="video/3gp">
+					</video>
+					<div class="details">
+						<h4>part 2</h4>
+						<h5>Multiple search</h5>
+					</div>
+				</article>
 
-			<article class="vignette">
-				<video width="100%" controls="controls">
-					<source src="video/video.webm" type="video/webm">
-					<source src="video/video.mp4" type="video/mp4">
-					<source src="video/video.ogv" type="video/ogg">
-					<source src="video/video.3gp" type="video/3gp">
-					Your browser does not support the video tag.
-				</video>
-				<div class="details">
-					<h4>part 2</h4>
-					<h5>Multiple search</h5>
-				</div>
-			</article>
-
-			<article class="vignette">
-				<video width="100%" controls="controls">
-					<source src="video/video.webm" type="video/webm">
-					<source src="video/video.mp4" type="video/mp4">
-					<source src="video/video.ogv" type="video/ogg">
-					<source src="video/video.3gp" type="video/3gp">
-					Your browser does not support the video tag.
-				</video>
-				<div class="details">
-					<h4>part 3</h4>
-					<h5>Multiple search</h5>
-				</div>
-			</article>
+				<article class="vignette" id="n10">
+					<video width="100%" controls="controls" poster="img/poster.png">
+						<source src="http://techslides.com/demos/sample-videos/small.webm"
+							type="video/webm">
+						<source src="http://techslides.com/demos/sample-videos/small.ogv"
+							type="video/ogg">
+						<source src="http://techslides.com/demos/sample-videos/small.mp4"
+							type="video/mp4">
+						<source src="http://techslides.com/demos/sample-videos/small.3gp"
+							type="video/3gp">
+					</video>
+					<div class="details">
+						<h4>part 3</h4>
+						<h5>Multiple search</h5>
+					</div>
+				</article>
+			</div>
+			<div style="clear:both;"></div>
 		</section>
 		
 		<section class="red">
-			<p><span>Almost done</span></p>
+			<p><span>Almost done !</span></p>
 			<p>Now that you a pretty good idea of what <span>D</span>ART is and can it do for you, let's do a quick roundup:</p>
 		</section>
 		
 		<section class="cream2">
+		<div id="triangle2"></div>
 			<h2>final round</h2>
 			
 			<h3><span>D</span>ART roundup</h3>
@@ -289,7 +314,7 @@
 				</div>
 
 				<div>
-					<p>What do the three columns in front of the query result list mean?</p>
+					<p>What do the three columns in front of the query result mean?</p>
 					<label for="answer1"> 
 						<input type="radio" name="question7" id="answer1" value="option1"> answer 1 
 					</label>
